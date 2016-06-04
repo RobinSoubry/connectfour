58043950be110a4da7ba98803860acabde22a926
Ed Mechem <ed@mechem.org>
2016-06-04 13:59:35 -0700

    Cleanup code

--- application.js
+++ application.js
@@ -1,66 +1,82 @@
+$(document).ready(function(){
+  var game = new NewGame;
+  // game.playGame();
 
-confirm("You are playing Connect 4, MVP edition");
+});
 
-var NewGame = function(){
- var currentGame = [["","","",""],
- ["","","",""],
- ["","","",""],
- ["","","",""]];
+//  JQuery
+//    play button event listener here
+//
+
+var NewGame = function() {
+  confirm("You are playing Connect 4, MVP edition");
+  this.currentGame = [
+    ["","","",""],
+    ["","","",""],
+    ["","","",""],
+    ["","","",""]
+  ];
 };
 
-NewGame.prototype.whoseTurn = funtion(){
-  var flattenedBoard = [].concat.apply([], currentGame);
-  
+NewGame.prototype.whoseTurn = function() {
+  var flattenedBoard = [].concat.apply([], this.currentGame);
+
   var redCount = 0;
   var blackCount = 0;
   var emptySpaces = 0;
-  
-  for(i = 0; i < flattenedBoard.length; i++){
-    if(flattenedBoard[i] === "R") {
-     redCount += 1
-   } else if (flattenedBoard[i] === "B") {
-     blackCount += 1  
-   } else {
-     emptySpaces += 1
-   }  
-  }
 
- if (redCount > blackCount){
-  currentColor = "B"
-} else {
-  currentColor = "R"
-}
-console.log(currentColor);
-return currentColor; 
+  for(i = 0; i < flattenedBoard.length; i++) {
+
+    if(flattenedBoard[i] === "R") {
+      redCount += 1
+    } else if (flattenedBoard[i] === "B") {
+      blackCount += 1
+    } else {
+      emptySpaces += 1
+    };
+
+  };
+
+  if (redCount > blackCount) {
+    currentColor = "B"
+  } else {
+    currentColor = "R"
+  };
+
+  console.log(currentColor);
+  return currentColor;
 };
 
-NewGame.prototype.playGame = function(){
+NewGame.prototype.playGame = function() {
 
   var currentColor = this.whoseTurn();
   var boardRow = 3;
 
-  for (i=0; i<10; i++){
+  for (i=0; i<10; i++) {
     var chosenColumn = prompt("Which column do you want?\nPick a number from 1 to 4");
     var placed = false;
+      while (placed === false) {
+      };
+      var slot = this.currentGame[boardRow][chosenColumn - 1];
 
-    while (placed === false){
-      var slot = currentGame[boardRow][chosenColumn - 1];
-      if (slot === ""){
-        currentGame[boardRow][chosenColumn - 1] = currentColor;
-        console.log(currentGame[boardRow][chosenColumn - 1]);
+      if (slot === "") {
+        this.currentGame[boardRow][chosenColumn - 1] = currentColor;
+        console.log(this.currentGame[boardRow][chosenColumn - 1]);
 
-        console.log(currentGame);
+        console.log(this.currentGame);
         placed = true;
-      }else{
-        if (boardRow == 0){
+      } else {
+
+        if (boardRow == 0) {
           alert("This column is full");
-          console.log(currentGame);
+          console.log(this.currentGame);
           placed = true;
-        }
-        else{
+        } else {
           boardRow -= 1;
-        }
-      }
-    }
-  }
+        };
+
+      };
+
+  };
+
 };
